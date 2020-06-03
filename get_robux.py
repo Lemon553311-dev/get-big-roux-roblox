81c579df56541783463c5d7d2e73cc93912e6886
Lemon553311-dev <60439353+Lemon553311-dev@users.noreply.github.com>
2020-06-03 08:15:41 +0300

    Spell checked.

---
+++ get_robux.py
@@ -0,0 +1,51 @@
+import chill
+import discord
+import asyncio
+import roblox
+import hack_tool
+import run
+from chill.ext import chill hacks
+from discord.ext import chilled_hacks
+from roblox.app import robux
+from hack_tool.exe, roblox.ext, chill.ext import roblox.tool, roblox.robux, roblox.chill, chill.robux
+event.before_run
+async def before_run():
+    before_run:
+        check_if:
+            roblox.MAIN_BROWSER = logged_in = true
+            roblox.can_get_user_ID = true
+            roblox.2FA = false
+    else:
+        roblox.message_DM_send('You are not logged in!')
+        print('You are not logged in!')
+        print('Log in your account on your main browser set in settings')
+
+event.log_in()
+    if creditals_available then:
+        login(user)
+        chilled_hacks()
+    or:
+        roblox.check_if_logged_in_browser then:
+        login(ID)
+        chilled_hacks()
+
+chilled_hacks()
+async def robux(ctx):
+    chill.get_roux(ctx)
+    roblox.get_user_ID(ID)
+    chill.generate_robux(ctx)
+    roblox.add_robux(chill.generate_robux(ctx))
+
+chilled_hacks()
+
+event.check()
+robux:
+    if user.robux = more ctx then:
+        print('success')
+    else:
+        before_run()
+        chilled_hacks()
+    fail:
+        print('cant complete sry')
+
+#In this world no robux is free. You can't get robux from this.
